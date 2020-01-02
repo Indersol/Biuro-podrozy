527b262661f062dac97fe8a7d705817a85fc7057
indersol000 <indersol000@o2.pl>
2020-01-02 16:00:33 +0100

    Utworzenie projektu

---
+++ Biuro Podróży/Models/BiuroContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biuro_Podróży.Models
+{
+    public class BiuroContext:DbContext
+{
+        public DbSet<User> User { get; set; }
+
+}
+}
---
+++ Biuro Podróży/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biuro_Podróży.Models
+{
+    public class User
+{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_usera { get; set; }
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane!")]
+        [DataType("Password")]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [DataType("Password")]
+        [Compare("Password", ErrorMessage = "Hasła nie są takie same!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Należy podać adres Email!")]
+        [EmailAddress(ErrorMessage = "Nie prawidłowy adres Email!")]
+        public string Email { get; set; }
+
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Miejscowosc { get; set; }
+        public string Telefon { get; set; }
+        public virtual ICollection<Uprawnienia> Id_uprawnien { get; set; }
+
+    }
+}
