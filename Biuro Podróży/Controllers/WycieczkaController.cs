91b51b4337f6d2dd0d3bfe08b62038d30e9c4363
indersol000 <indersol000@o2.pl>
2020-01-03 16:11:18 +0100

    emm

---
+++ Biuro Podróży/Controllers/WycieczkaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biuro_Podróży.Controllers
+{
+    public class WycieczkaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
