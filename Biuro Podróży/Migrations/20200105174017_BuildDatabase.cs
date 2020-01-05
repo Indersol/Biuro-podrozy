1c7d937e50d66a984fee5f011f05e5cec587f2b4
indersol <indersol000@o2.pl>
2020-01-05 18:41:43 +0100

    Zmiana sposobu autoryzacji na Identity Na razie dodana rejestracja

---
+++ Biuro Podróży/Migrations/20200105174017_BuildDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BiuroPodróży.Migrations
+{
+    public partial class BuildDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
