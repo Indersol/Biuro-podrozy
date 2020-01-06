e6a1614b3d64f389f6b37e5db599b15b29b9c837
indersol <indersol000@o2.pl>
2020-01-06 04:20:34 +0100

    Dodanie rezerwacji

---
+++ Biuro Podróży/Controllers/RezerwacjeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Biuro_Podróży.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biuro_Podróży.Controllers
+{
+    public class RezerwacjeController : Controller
+    {
+        private readonly BiuroContext _context;
+        public RezerwacjeController(BiuroContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public IActionResult Create(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create(int id, [Bind("Id_zamowienia,Id_usera,Id_wycieczki,Bilety")] Wycieczka_Klient wycieczka_Klient)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //wyciąganie id z IdentityUser czy raczej ApplicationUser po zmianie
+                ClaimsPrincipal currentUser = this.User;
+                wycieczka_Klient.Id = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                wycieczka_Klient.Id_wycieczki = id;
+                _context.Add(wycieczka_Klient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("index", "Wycieczka");
+            }
+
+            ViewData["Id_wycieczki"] = id;
+            ViewBag.Message = " Dodany to agregatora!";
+            return View(wycieczka_Klient);
+        }
+    }
+}
